611b8a6e5f5dc46d1aaa00c4bbf42bccc1d26b32
waxei <yuwakusei@gmail.com>
2024-12-15 22:57:56 +0400

    Решение для Task 2

--- robots/views.py
+++ robots/views.py
@@ -1,11 +1,13 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
-
 from django.db import IntegrityError
-
-
 from .models import Robot
 import json
+from django.utils import timezone
+from io import BytesIO
+from django.db.models import Count
+import openpyxl
+
 
 # View для создания робота
 class RobotCreateView(View):
@@ -42,3 +44,37 @@ class RobotCreateView(View):
             return JsonResponse({"error": f"Ошибка целостности данных: {str(e)}"}, status=400)
         except Exception as e:
             return JsonResponse({"error": f"Произошла ошибка: {str(e)}"}, status=500)
+
+
+class RobotReportView(View):
+    def get(self, request):
+        last_week = timezone.now() - timezone.timedelta(days=7)
+        robots = Robot.objects.filter(created__gte=last_week)
+
+        if not robots.exists():
+            return JsonResponse({"error": "Нет данных для генерации отчета за последнюю неделю."}, status=404)
+
+        try:
+            workbook = openpyxl.Workbook()
+            default_sheet = workbook.active
+            workbook.remove(default_sheet)
+            grouped_data = robots.values('model', 'version').annotate(count=Count('id'))
+
+            models = grouped_data.values_list('model', flat=True).distinct()
+            for model in models:
+                worksheet = workbook.create_sheet(title=model)
+                worksheet.append(['Модель', 'Версия', 'Количество за неделю'])
+
+                for item in grouped_data.filter(model=model):
+                    worksheet.append([item['model'], item['version'], item['count']])
+
+            file_buffer = BytesIO()
+            workbook.save(file_buffer)
+            file_buffer.seek(0)
+
+            response = HttpResponse(file_buffer, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            response['Content-Disposition'] = 'attachment; filename="weekly_report.xlsx"'
+            return response
+
+        except Exception as e:
+            return JsonResponse({"error": f"Произошла ошибка при генерации отчета: {str(e)}"}, status=500)
